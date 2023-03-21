155f60500492267f51d0dbcafd828281b3ef6cb3
Bravo1B9 <brofe93@gmail.com>
2023-03-21 13:28:37 +0000

    Complete functionality

--- magic-8-ball/src/app/home/home.page.ts
+++ magic-8-ball/src/app/home/home.page.ts
@@ -16,7 +16,28 @@ export class HomePage {
   }
 
   shake() {
-    this.answer = "Yes"
+    const answers = [
+      "It is certain.",
+      "It is decidedly so.",
+      "Without a doubt.",
+      "Yes - definitely.",
+      "You may rely on it.",
+      "As I see it, yes.",
+      "Most likely.",
+      "Outlook good.",
+      "Yes.",
+      "Signs point to yes.",
+      "Reply hazy, try again.",
+      "Ask again later.",
+      "Better not tell you now.",
+      "Cannot predict now.",
+      "Concentrate and ask again.",
+      "Don't count on it.",
+      "Outlook not so good.",
+      "My sources say no.",
+      "Very doubtful.",
+    ];
+    this.answer = answers[Math.floor(Math.random() * answers.length)];
   }
 
 }
