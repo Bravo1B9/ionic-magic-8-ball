c54484a4f37fcf81bca76677f64bfa747f922be3
Bravo1B9 <brofe93@gmail.com>
2023-03-21 12:17:33 +0000

    Initial setup

---
+++ magic-8-ball/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'magic-8-ball',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
